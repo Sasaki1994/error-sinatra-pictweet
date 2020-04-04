127b09e72ca3c7ea687caa8d70a3b9f59d4f872d
Sasaki1994 <b.92421933411@gmail.com>
2020-04-04 10:31:51 +0000

    fail Tweet.create

--- pictweet.rb
+++ pictweet.rb
@@ -3,7 +3,7 @@ require 'bundler'
 
 Bundler.require
 
-set :database, {adapter: "sqlite3", database: "tweets.sqlite3"}
+set :database, {adopter: "sqlite3", database: "tweets.sqlite3"}
 
 class Tweet < ActiveRecord::Base
 end
