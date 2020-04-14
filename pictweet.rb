6bc67286a872cfef47b039918e1d600fad072203
Sasaki1994 <b.92421933411@gmail.com>
2020-04-14 02:55:25 +0000

    revise

--- pictweet.rb
+++ pictweet.rb
@@ -28,7 +28,7 @@ get "/tweets/:id/edit" do
 end
 
 patch '/tweets/:id' do
-  tweet = Tweet.find(text: params[:txt], image: params[:image])
-  tweet.update()
+  tweet = Tweet.find(params[:id])
+  tweet.update(text: params[:txt], image: params[:image])
   redirect '/'
 end
